15b5fa50a10d8ac231195946b5a474ef4d25a6d6
tad <tad@L404.hicron.local>
2015-02-26 09:44:54 +0100

    fori1

--- WebContent/Component.js
+++ WebContent/Component.js
@@ -12,6 +12,12 @@ sap.ui.core.UIComponent.extend("sap.ui.demo.myFiori.Component", {
 			viewData : { component : this }
 		});
 
+		// set i18n model
+		var i18nModel = new sap.ui.model.resource.ResourceModel({
+			bundleUrl : "i18n/messageBundle.properties"
+		});
+		oView.setModel(i18nModel, "i18n");
+
 //		// Using OData model to connect against a real service
 //		var url = "/proxy/http/<server>:<port>/sap/opu/odata/sap/ZGWSAMPLE_SRV/";
 //		var oModel = new sap.ui.model.odata.ODataModel(url, true, "<user>", "<password>");
